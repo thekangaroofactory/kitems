884cc8aa68efe7455f4741dc6c340fcf5239d5b7
kangarooaifr <philippe.peret@kangaroo-ai.fr>
2023-02-26 11:42:33 +0100

    Intermediate code review

--- R/kitems_module_server.R
+++ R/kitems_module_server.R
@@ -1,87 +1,161 @@
 
 
-# --------------------------------------------------------------------------------
-# Shiny module: Data
-# --------------------------------------------------------------------------------
+# *****************************************
+# - add user notifications when Shiny session
+# - add parameter to trun autosave off (default ON) : just add if(TRUE) devant observerEvent
+# *****************************************
 
-# -- Library
-library(DT)
-library(stringr)
 
-# -------------------------------------
-# Server logic
-# -------------------------------------
+# ------------------------------------------------------------------------------
+# Shiny module server logic
+# ------------------------------------------------------------------------------
 
-kitemsManager_Server <- function(id, r, path, file = NULL, col.classes = NULL, filter.cols = NULL) {
+# -- function definition
+kitemsManager_Server <- function(id, r, file, path, col.classes = NULL, filter.cols = NULL, create = TRUE) {
   moduleServer(id, function(input, output, session) {
 
+    # --------------------------------------------------------------------------
+    # Declare config parameters:
+    # --------------------------------------------------------------------------
 
-    # -------------------------------------
+    # -- Build log pattern
+    MODULE <- paste0("[", id, "]")
+
+    # -- Object types
+    LIST_OBJECT_TYPES <- c("NA", "NULL",
+                           "numeric", "integer", "complex", "logical", "character", "raw",
+                           "double", "factor", "Date", "POSIXct", "POSIXlt")
+
+
+    # --------------------------------------------------------------------------
     # Init:
-    # -------------------------------------
+    # --------------------------------------------------------------------------
 
-    cat(paste0("-- [", id, "] Starting kitems module server... \n"))
+    cat(MODULE, "Starting kitems module server... \n")
 
-    # get namespace
+    # -- Get namespace
     ns <- session$ns
 
-    # declare communication objects
+
+    # -- Build object names from module id (to access outside module)
     r_items <- paste0(id, "_items")
     #r_raw_table <- paste0(id, "_raw_table")
     #r_table <- paste0(id, "_table")
 
-    # declare reactive objects (internal)
-    colClasses <- reactiveVal(NULL)
-    filter_cols <- reactiveVal(NULL)
-
-    # declare triggers
+    # -- Build triggers names from module id (to access outside module)
     trigger_add <- paste0(id, "_trigger_new")
     # trigger_update
     # trigger_delete
 
-    # declare config parameters
-    CREATE_FILE <- FALSE
 
-    COL_CLASSES_LIST <- c("integer", "double", "character", "Date", "POSIXct", "logical")
+    # -- Declare reactive objects (for internal use)
+    colClasses <- reactiveVal(NULL)
+    filter_cols <- reactiveVal(NULL)
 
+    # -- Declare reactive objects (for external use)
+    r[[trigger_add]] <- reactiveVal(NULL)
+
+
+    # **************************************************************************
+    # -- Sample data, export it somewhere!
+    # **************************************************************************
     TEMPLATE_COLS <- data.frame(name = c("date", "target.date",
                                          "name", "title", "description", "comment", "status",
                                          "debit", "credit", "total", "balance", "progress"),
                                 type = c("Date", "POSIXct",
                                          rep("character", 5),
                                          rep("double", 4), "integer"))
+    # **************************************************************************
 
 
-    # -------------------------------------
+    # --------------------------------------------------------------------------
     # Load Resources:
-    # -------------------------------------
+    # --------------------------------------------------------------------------
 
+    # *************************************
+    # check that again:
+    # what happens if not given as argument
+    # **************************************
+
+    # -- colClasses
     target_url <- file.path(path$resource, paste0(id, "_colClasses.rds"))
     if(file.exists(target_url))
       col.classes <- readRDS(target_url)
 
+    colClasses(col.classes)
+
+    # save colClasses
+    observeEvent(colClasses(), {
+
+      # -- Write & notify
+      saveRDS(colClasses(), file = file.path(path$resource, paste0(id, "_colClasses.rds")))
+      cat(MODULE, "colClasses saved \n")
+
+    }, ignoreInit = TRUE)
+
+
+    # -- Filters
     target_url <- file.path(path$resource, paste0(id, "_filterCols.rds"))
     if(file.exists(target_url))
       filter.cols <- readRDS(target_url)
 
+    filter_cols(filter.cols)
 
-    # -------------------------------------
-    # Load data:
-    # -------------------------------------
 
-    # -- try load (returns NULL if not found)
+    # save filter_cols
+    observeEvent(filter_cols(), {
+
+      # -- Write & notify
+      saveRDS(filter_cols(), file = file.path(path$resource, paste0(id, "_filterCols.rds")))
+      cat(MODULE, "filter_cols saved \n")
+
+    }, ignoreInit = TRUE, ignoreNULL = FALSE)
+
+
+    # --------------------------------------------------------------------------
+    # Load the data:
+    # --------------------------------------------------------------------------
+
+    # -- Try load (see read_data for details about returns)
     items <- kfiles::read_data(file = file,
                                path = path$data,
                                colClasses = col.classes,
-                               create = CREATE_FILE)
+                               create = create)
 
-    # -- store objects
+    # -- Notify
+    cat(MODULE, "Item list loaded \n")
+
+    # -- Store into communication objects
     r[[r_items]] <- reactiveVal(items)
-    colClasses(col.classes)
-    filter_cols(filter.cols)
 
-    # -- init triggers
-    r[[trigger_add]] <- reactiveVal(NULL)
+
+    # --------------------------------------------------------------------------
+    # Auto save the data:
+    # --------------------------------------------------------------------------
+
+    # -- Save items
+    observeEvent(r[[r_items]](), {
+
+      # -- Write
+      kfiles::write_data(data = r[[r_items]](),
+                         file = file,
+                         path = path$data)
+
+      # -- Notify
+      cat(MODULE, "[Auto] item list saved \n")
+
+    }, ignoreInit = TRUE)
+
+
+
+
+
+
+
+
+    # ****************************************************************************
+    # *** CODE REVIEW : END done
+    # ****************************************************************************
 
 
 
@@ -108,7 +182,7 @@ kitemsManager_Server <- function(id, r, path, file = NULL, col.classes = NULL, f
         # apply column name mask
         colnames(value) <- gsub(".", " ", colnames(value), fixed = TRUE)
         colnames(value) <- gsub("_", " ", colnames(value), fixed = TRUE)
-        colnames(value) <- str_to_title(colnames(value))
+        colnames(value) <- stringr::str_to_title(colnames(value))
 
       } else value <- NULL
 
@@ -123,52 +197,25 @@ kitemsManager_Server <- function(id, r, path, file = NULL, col.classes = NULL, f
     # -------------------------------------
 
     # Raw view for admin
-    output$raw_item_table <- renderDT(r[[r_items]](),
-                                 rownames = FALSE,
-                                 options = list(lengthMenu = c(5, 10, 15), pageLength = 5, dom = "ltp"),
-                                 selection = list(mode = 'single', target = "row", selected = NULL))
+    output$raw_item_table <- DT::renderDT(r[[r_items]](),
+                                          rownames = FALSE,
+                                          options = list(lengthMenu = c(5, 10, 15), pageLength = 5, dom = "ltp"),
+                                          selection = list(mode = 'single', target = "row", selected = NULL))
 
     # Masked view for admin
-    output$view_item_table <- renderDT(view_items(),
-                                       rownames = FALSE,
-                                       options = list(lengthMenu = c(5, 10, 15), pageLength = 5, dom = "ltp"),
-                                       selection = list(mode = 'single', target = "row", selected = NULL))
+    output$view_item_table <- DT::renderDT(view_items(),
+                                           rownames = FALSE,
+                                           options = list(lengthMenu = c(5, 10, 15), pageLength = 5, dom = "ltp"),
+                                           selection = list(mode = 'single', target = "row", selected = NULL))
 
 
     # Masked view for user
     # TODO: add user view (add function params for renderDT...)
 
 
-    # -------------------------------------
-    # Auto save:
-    # -------------------------------------
-
-    # save items
-    observeEvent(r[[r_items]](), {
-
-      kfiles::write_data(data = r[[r_items]](),
-                         file = file,
-                         path = path$data)
-
-    }, ignoreInit = TRUE)
 
 
-    # save colClasses
-    observeEvent(colClasses(), {
 
-      cat("[SAVE] colClasses \n")
-      saveRDS(colClasses(), file = file.path(path$resource, paste0(id, "_colClasses.rds")))
-
-    }, ignoreInit = TRUE)
-
-
-    # save filter_cols
-    observeEvent(filter_cols(), {
-
-      cat("[SAVE] filter_cols \n")
-      saveRDS(filter_cols(), file = file.path(path$resource, paste0(id, "_filterCols.rds")))
-
-    }, ignoreInit = TRUE, ignoreNULL = FALSE)
 
 
     # -------------------------------------
@@ -178,50 +225,50 @@ kitemsManager_Server <- function(id, r, path, file = NULL, col.classes = NULL, f
     # -- define inputs
     output$action_buttons <- renderUI({
 
-        # check
-        if(is.null(r[[r_items]]())){
+      # check
+      if(is.null(r[[r_items]]())){
 
-          # create data
-          actionButton(ns("create_data"), label = "Create")
+        # create data
+        actionButton(ns("create_data"), label = "Create")
 
-        } else {
+      } else {
 
-          tagList(
+        tagList(
 
-            # add column name
-            selectizeInput(inputId = ns("add_col_name"),
-                           label = "Name",
-                           choices = TEMPLATE_COLS$name[!TEMPLATE_COLS$name %in% colnames(r[[r_items]]())],
-                           selected = NULL,
-                           options = list(create = TRUE,
-                                          placeholder = 'Type or select an option below',
-                                          onInitialize = I('function() { this.setValue(""); }'))),
+          # add column name
+          selectizeInput(inputId = ns("add_col_name"),
+                         label = "Name",
+                         choices = TEMPLATE_COLS$name[!TEMPLATE_COLS$name %in% colnames(r[[r_items]]())],
+                         selected = NULL,
+                         options = list(create = TRUE,
+                                        placeholder = 'Type or select an option below',
+                                        onInitialize = I('function() { this.setValue(""); }'))),
 
-            # add column type
-            selectizeInput(inputId = ns("add_col_type"),
-                           label = "Type",
-                           choices = COL_CLASSES_LIST,
-                           selected = NULL,
-                           options = list(create = FALSE,
-                                          placeholder = 'Type or select an option below',
-                                          onInitialize = I('function() { this.setValue(""); }'))),
+          # add column type
+          selectizeInput(inputId = ns("add_col_type"),
+                         label = "Type",
+                         choices = LIST_OBJECT_TYPES,
+                         selected = NULL,
+                         options = list(create = FALSE,
+                                        placeholder = 'Type or select an option below',
+                                        onInitialize = I('function() { this.setValue(""); }'))),
 
-            # add column button
-            actionButton(ns("add_col"), label = "Add column"),
+          # add column button
+          actionButton(ns("add_col"), label = "Add column"),
 
-            # order column name
-            selectizeInput(inputId = ns("order_cols"),
-                           label = "Select cols order",
-                           choices = colnames(r[[r_items]]()),
-                           selected = colnames(r[[r_items]]()),
-                           multiple = TRUE),
+          # order column name
+          selectizeInput(inputId = ns("order_cols"),
+                         label = "Select cols order",
+                         choices = colnames(r[[r_items]]()),
+                         selected = colnames(r[[r_items]]()),
+                         multiple = TRUE),
 
-            # order column button
-            actionButton(ns("sort_col"), label = "Reorder")
+          # order column button
+          actionButton(ns("sort_col"), label = "Reorder")
 
-            ) # end tagList
+        ) # end tagList
 
-        }})
+      }})
 
 
     # -- update add_col_type given add_col_name
@@ -229,7 +276,7 @@ kitemsManager_Server <- function(id, r, path, file = NULL, col.classes = NULL, f
 
       updateSelectizeInput(session = session,
                            inputId = "add_col_type",
-                           choices = COL_CLASSES_LIST,
+                           choices = LIST_OBJECT_TYPES,
                            selected = TEMPLATE_COLS[TEMPLATE_COLS$name == input$add_col_name, ]$type)})
 
 
@@ -309,7 +356,7 @@ kitemsManager_Server <- function(id, r, path, file = NULL, col.classes = NULL, f
                                     placeholder = 'Type or select an option below',
                                     onInitialize = onInitialize))
 
-        })
+    })
 
     # observe filter input
     observeEvent(input$filter_col, {
