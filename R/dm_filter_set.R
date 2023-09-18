31e2f49f04f9dbeb5a8590b772b6657074a976dc
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2023-09-18 21:45:18 +0200

    Fixed #70

--- R/dm_filter_set.R
+++ R/dm_filter_set.R
@@ -11,11 +11,16 @@
 #' @examples
 
 
-dm_filter_set <- function(data.model = dm, filter = input$filter_col){
+dm_filter_set <- function(data.model, filter){
 
-  # -- Reset filter column & update
-  data.model$filter <- FALSE
-  data.model[match(filter, data.model$name), ]$filter <- TRUE
+  # -- Test NULL
+  if(!is.null(data.model) & !is.null(filter)){
+
+    # -- Reset filter column & update
+    data.model$filter <- FALSE
+    data.model[match(filter, data.model$name), ]$filter <- TRUE
+
+  }
 
   # -- return
   data.model
