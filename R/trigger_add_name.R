008f47e3f60bc6c7a7a1c2afa66e7070b92edfb1
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-01-11 10:38:34 +0100

    Fixed #107

---
+++ R/trigger_add_name.R
@@ -0,0 +1,17 @@
+
+
+#' Title
+#'
+#' @param id
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+
+trigger_add_name <- function(id){
+
+  paste0(id, "_trigger_add")
+
+}
---
+++ R/trigger_save_name.R
@@ -0,0 +1,17 @@
+
+
+#' Title
+#'
+#' @param id
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+
+trigger_save_name <- function(id){
+
+  paste0(id, "_trigger_save")
+
+}
