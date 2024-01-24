24c978da97138a1a0332f68703d84f398037a656
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-01-24 16:28:08 +0100

    Test coverage #154

--- tests/testthat/test-data_model.R
+++ tests/testthat/test-data_model.R
@@ -1,12 +1,55 @@
 
 
-test_that("data_model: colClasses argument only", {
+# ------------------------------------------------------------------------------
+# Init
+# ------------------------------------------------------------------------------
 
-  # -- init
-  colClasses <- c(id = "numeric", date = "POSIXct", name = "character", isvalid = "logical")
+# -- declare colClasses
+colClasses <- c(id = "numeric", date = "POSIXct", name = "character", isvalid = "logical")
+
+# -- declare default.val
+default_val <- c("name" = "test", "isvalid" = TRUE)
+
+# -- declare default.fun
+default_fun <- c("date" = "Sys.Date")
+
+
+# ------------------------------------------------------------------------------
+# Tests
+# ------------------------------------------------------------------------------
+
+test_that("data_model: colClasses argument only", {
 
   # -- function call
   dm <- data_model(colClasses = colClasses, default.val = NULL, default.fun = NULL, filter = NULL, skip = NULL)
+
+  # -- check: output is data.frame
+  expect_output(str(dm), "data.frame")
+
+  # -- check: output dim
+  expect_equal(dim(dm), c(4,6))
+
+})
+
+
+test_that("data_model: default.val", {
+
+  # -- function call
+  dm <- data_model(colClasses = colClasses, default.val = default_val, default.fun = NULL, filter = NULL, skip = NULL)
+
+  # -- check: output is data.frame
+  expect_output(str(dm), "data.frame")
+
+  # -- check: output dim
+  expect_equal(dim(dm), c(4,6))
+
+})
+
+
+test_that("data_model: default.fun", {
+
+  # -- function call
+  dm <- data_model(colClasses = colClasses, default.val = NULL, default.fun = default_fun, filter = NULL, skip = NULL)
 
   # -- check: output is data.frame
   expect_output(str(dm), "data.frame")
