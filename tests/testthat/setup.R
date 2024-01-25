708ede38ba6a35638ece89a8ef2e4bc33f946f8b
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-01-25 11:35:25 +0100

    Test coverage #154

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -7,6 +7,7 @@
 # -- declare colClasses
 colClasses <- c(id = "numeric", date = "POSIXct", name = "character", isvalid = "logical")
 colClasses_extra_att <- c(id = "numeric", date = "POSIXct", name = "character", isvalid = "logical", extra_att = "integer")
+colClasses_no_date <- c(id = "numeric", name = "character", isvalid = "logical")
 
 # -- declare default.val
 default_val <- c("name" = "test", "isvalid" = TRUE)
@@ -32,6 +33,7 @@ r <- reactiveValues(dm1_data_model = 1, dm2_data_model = 2)
 dm <- data_model(colClasses = colClasses, default.val = default_val, default.fun = default_fun, filter = filter, skip = skip)
 dm_nofilter <- data_model(colClasses = colClasses, default.val = default_val, default.fun = default_fun, filter = NULL, skip = skip)
 dm_extra_att <- data_model(colClasses = colClasses_extra_att, default.val = default_val, default.fun = default_fun, filter = filter, skip = skip)
+dm_no_date<- data_model(colClasses = colClasses_no_date)
 
 # -- items
 items <- data.frame("id" = c(1705158971950, 1705313192780, 1705313216662, 1705399423521),
