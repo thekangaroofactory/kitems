b94297cc9d5c72e8a7d31ae5f152c7647094a717
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-01-31 10:03:39 +0100

    Adding coverage: reorder & filter data model

--- tests/testthat/test-kitemsManager_Server.R
+++ tests/testthat/test-kitemsManager_Server.R
@@ -238,6 +238,96 @@ test_that("Add attribute works", {
 
 
 # --------------------------------------------------------------------------
+# Scenario: reorder & filter data model cols
+# --------------------------------------------------------------------------
+
+test_that("Server works", {
+
+  cat("\n-------------------------------------------------------------------------- \n")
+  cat("Scenario: reorder & filter data model cols")
+  cat("\n-------------------------------------------------------------------------- \n")
+
+  # -- declare arguments
+  params <- list(id = module_id,
+                 r = r,
+                 file = "my_data.csv",
+                 path = test_path,
+                 data.model = dm,
+                 create = FALSE,
+                 autosave = TRUE)
+
+  # -- module server call
+  testServer(kitemsManager_Server, args = params, {
+
+    # -- update input
+    session$setInputs(dm_order_cols = names(colClasses[order(names(colClasses))]))
+
+
+    # --------------------------------------------------------------------------
+    # Data model
+    # --------------------------------------------------------------------------
+
+    r_data_model <- dm_name(module_id)
+    x <- r[[r_data_model]]()
+
+    # -- test class
+    expect_s3_class(x, "data.frame")
+
+    # -- test dim
+    expect_equal(dim(x), dim(dm))
+
+    # -- test names
+    expect_equal(x$name, names(colClasses[order(names(colClasses))]))
+
+
+    # --------------------------------------------------------------------------
+    # Items
+    # --------------------------------------------------------------------------
+
+    r_items <- items_name(module_id)
+    x <- r[[r_items]]()
+
+    # -- test class
+    expect_s3_class(x, "data.frame")
+
+    # -- test dim
+    expect_equal(dim(x), dim(items))
+
+    # -- test names
+    expect_equal(colnames(x), names(colClasses[order(names(colClasses))]))
+
+
+    # --------------------------------------------------------------------------
+    # filter cols
+    # --------------------------------------------------------------------------
+
+    # -- update input
+    session$setInputs(adm_filter_col = c("id", "total"))
+
+
+    # --------------------------------------------------------------------------
+    # Data model
+    # --------------------------------------------------------------------------
+
+    r_data_model <- dm_name(module_id)
+    x <- r[[r_data_model]]()
+
+    # -- test class
+    expect_s3_class(x, "data.frame")
+
+    # -- test dim
+    expect_equal(dim(x), dim(dm))
+
+    # -- test names
+    expect_equal(x$name[x$filter], c("id", "total"))
+
+
+  })
+
+})
+
+
+# --------------------------------------------------------------------------
 # Cleanup
 # --------------------------------------------------------------------------
 
