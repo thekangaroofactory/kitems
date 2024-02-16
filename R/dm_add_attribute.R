84fc6da1435f86bac4f9dc0b3f79c5c227ccd3d5
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-02-16 14:42:30 +0100

    Fixed #230

--- R/dm_add_attribute.R
+++ R/dm_add_attribute.R
@@ -35,7 +35,8 @@ dm_add_attribute <- function(data.model, name, type, default.val = NA, default.f
     default.fun <- NA
 
   # -- make sure default.val & fun are mutual exclusive #230
-  if(!is.na(default.fun))
+  # limitation: to when default.fun contains only 1 value (one attribute is added at a time)
+  if(length(default.fun) == 1 && !is.na(default.fun))
     default.val <- NA
 
   # -- Check for NULL
