e28355071ad63f83097797b8bd319c677534faec
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-02-16 18:09:49 +0100

    Adding test coverage #227

--- tests/testthat/test-item_check_integrity.R
+++ tests/testthat/test-item_check_integrity.R
@@ -15,3 +15,31 @@ test_that("item_check_integrity works", {
   expect_equal(class(x$date), "Date")
 
 })
+
+
+test_that("item_check_integrity / NULL items works", {
+
+  # -- function call
+  x <- item_check_integrity(items = NULL, data.model = dm)
+
+  # -- test
+  expect_null(x)
+
+})
+
+
+test_that("item_check_integrity / error works", {
+
+  # -- generate mismatch to solve
+  items$date <- "dummy_string"
+
+  # -- alter dm
+  dm[dm$name == "date", ]$type <- "Date"
+
+  # -- function call
+  x <- item_check_integrity(items = items, data.model = dm)
+
+  # -- test
+  expect_equal(class(x$date), "character")
+
+})
