b0b2d708e021bc82b43733d9897e71728da35ae0
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-10-02 17:53:11 +0200

    Fixed #286

--- R/dm_filter_set.R
+++ R/dm_filter_set.R
@@ -1,11 +1,11 @@
 
 
-#' Set data model attribute filter
+#' Data model attribute filter
 #'
 #' @param data.model a data.frame containing the data model
-#' @param filter a character vector with the name of the attributes to be filtered
+#' @param set an optional character vector with the name of the attributes to set as filtered
 #'
-#' @return an updated data model
+#' @return either the list of attributes that are filtered or an updated data model if set is not NULL
 #' @export
 #'
 #' @details
@@ -13,33 +13,49 @@
 #'
 #' @examples
 #' \dontrun{
-#' dm_filter_set(data.model = mydatamodel, filter = c("id", "internal"))
+#' dm_filter(data.model = mydatamodel, filter = c("id", "internal"))
 #' }
 
 
-dm_filter_set <- function(data.model, filter){
+dm_filter <- function(data.model, set = NULL){
 
-  # -- test filter
-  if(!is.null(filter)){
+  # -- test set
+  if(!is.null(set)){
 
-    # -- current filters
+    # -- current filters, attributes to set / unset
     current_filter <- data.model[data.model$filter, ]$name
+    att_set <- set[!set %in% current_filter]
+    att_unset <- current_filter[!current_filter %in% set]
 
-    # -- attributes to filter
-    att_set <- filter[!filter %in% current_filter]
+    # -- filter
     if(length(att_set) > 0){
       cat("-- filter to set =", att_set, "\n")
       data.model[data.model$name %in% att_set, ]$filter <- TRUE}
 
-    # -- attributes to un-filter
-    att_unset <- current_filter[!current_filter %in% filter]
+    # -- un-filter
     if(length(att_unset) > 0){
       cat("-- filter to unset =", att_unset, "\n")
       data.model[data.model$name %in% att_unset, ]$filter <- FALSE}
 
-  }
+    # -- return
+    return(data.model)
 
-  # -- Return
-  data.model
+  } else {
+
+    # -- check data model
+    if(is.null(data.model))
+      return(NULL)
+
+    # -- get names where filter TRUE
+    filter <- data.model[data.model$filter, ]$name
+
+    # -- check (no filter in data model)
+    if(identical(filter, character(0)))
+      filter <- NULL
+
+    # -- return
+    return(filter)
+
+  }
 
 }
