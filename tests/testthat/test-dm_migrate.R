1899653d17d5df7ab4b2a065149a889353eb7660
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-10-04 14:39:11 +0200

    Item ordering impacts #239

--- tests/testthat/test-dm_migrate.R
+++ tests/testthat/test-dm_migrate.R
@@ -4,11 +4,7 @@
 data.model <- data.frame(name = c("id", "name"),
                          type = c("numeric", "character"))
 
-missing_col <- c("default.val",
-                 "default.fun",
-                 "default.arg",
-                 "filter",
-                 "skip")
+missing_col <- names(DATA_MODEL_COLCLASSES[!names(DATA_MODEL_COLCLASSES) %in% names(data.model)])
 
 
 test_that("dm_migrate works", {
