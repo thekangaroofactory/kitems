1a9a96f5882d900aeb9046cfd1c29f1873e5075d
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-10-14 15:31:12 +0200

    danger zone #299

---
+++ tests/testthat/test-server_danger_zone.R
@@ -0,0 +1,34 @@
+
+
+# -- Setup
+create_testdata()
+
+
+# -- Scenario:
+test_that("[testServer] Admin UI danger zone works", {
+
+  # -- declare arguments
+  params <- list(id = module_id,
+                 path = testdata_path,
+                 create = FALSE,
+                 autosave = TRUE)
+
+  # -- module server call
+  testServer(kitemsManager_Server, args = params, {
+
+    # -- flush reactive values
+    session$flushReact()
+
+    # -- update input
+    session$setInputs(adm_dz_toggle = TRUE)
+
+    # -- test output
+    expect_type(output$dm_danger_zone, "list")
+
+  })
+
+})
+
+
+# -- Cleanup
+clean_all(testdata_path)
