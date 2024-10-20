0f7bd680e916aec7b3a51ff527c9c36bba8d5fc8
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-10-20 15:37:23 +0200

    QA coverage

---
+++ tests/testthat/test-server_attribute_wizard_update.R
@@ -0,0 +1,48 @@
+
+
+# -- Setup
+create_testdata()
+
+
+# -- Scenario:
+test_that("[testServer] Update attribute wizard works", {
+
+  # -- declare arguments
+  params <- list(id = module_id,
+                 path = testdata_path,
+                 create = FALSE,
+                 autosave = TRUE,
+                 admin = TRUE)
+
+  # -- module server call
+  testServer(kitems_server, args = params, {
+
+    # -- flush reactive values
+    session$flushReact()
+
+    # -- update inputs
+    # case: character attribute
+    session$setInputs('admin-dm_table_rows_selected' = 3)
+    session$setInputs('admin-update_attribute' = 1)
+
+    # -- case: no default
+    session$setInputs('admin-wizard-w_default_choice' = "none")
+    session$setInputs('admin-wizard-w_set_sf' = 1)
+    session$setInputs('admin-wizard-w_skip' = FALSE)
+    session$setInputs('admin-wizard-w_filter' = FALSE)
+    session$setInputs('admin-wizard-w_set_sort' = 1)
+    session$setInputs('admin-wizard-w_sort' = FALSE)
+    session$setInputs('admin-wizard-w_ask_confirm' = 1)
+    session$setInputs('admin-wizard-w_confirm' = 1)
+
+    # - tests
+    expect_true("name" %in% k_data_model()$name)
+    expect_true("name" %in% names(k_items()))
+
+  })
+
+})
+
+
+# -- Cleanup
+clean_all(testdata_path)
