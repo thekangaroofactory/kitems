5c62cb427fbb59755bc205159fc35839f379583f
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-10-20 14:48:12 +0200

    QA coverage

---
+++ tests/testthat/test-server_date_slider_remove_date.R
@@ -0,0 +1,55 @@
+
+
+# --------------------------------------------------------------------------
+# Setup
+# --------------------------------------------------------------------------
+
+create_testdata()
+
+
+# --------------------------------------------------------------------------
+# Scenario: date sliderInput remove date attribute
+# --------------------------------------------------------------------------
+
+test_that("Date sliderInput remove date attribute works", {
+
+  cat("\n-------------------------------------------------------------------------- \n")
+  cat("Scenario: date sliderInput remove date attribute")
+  cat("\n-------------------------------------------------------------------------- \n")
+
+  # -- declare arguments
+  params <- list(id = module_id,
+                 path = testdata_path,
+                 create = FALSE,
+                 autosave = TRUE,
+                 admin = TRUE)
+
+  # -- module server call
+  testServer(kitems_server, args = params, {
+
+    # --------------------------------------------------------------------------
+    # date
+    # --------------------------------------------------------------------------
+
+    # -- update input
+    session$setInputs(date_slider_strategy = "this-year")
+    session$setInputs(date_slider = date_slider_value)
+
+    # -- update input & click
+    session$setInputs('admin-dz_delete_att_name' = "date")
+    session$setInputs('admin-dz_delete_att' = 1)
+    session$setInputs('admin-dz_delete_att_confirm' = 1)
+
+    # -- check
+    expect_null(filter_date())
+
+  })
+
+})
+
+
+# --------------------------------------------------------------------------
+# Cleanup
+# --------------------------------------------------------------------------
+
+clean_all(testdata_path)
