936af4233e1da1cd293b7896baf86538282685fe
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-10-20 12:23:47 +0200

    delete dm autosave off

---
+++ tests/testthat/test-server_dz_delete_dm_autosave_off.R
@@ -0,0 +1,41 @@
+
+
+# -- Setup
+create_testdata()
+
+
+# -- Scenario:
+test_that("[testServer] Delete data model works", {
+
+  # -- declare arguments
+  params <- list(id = module_id,
+                 path = testdata_path,
+                 create = FALSE,
+                 autosave = FALSE,
+                 admin = TRUE)
+
+  # -- module server call
+  testServer(kitems_server, args = params, {
+
+    # -- flush reactive values
+    session$flushReact()
+
+    # -- update input
+    session$setInputs('admin-dz_delete_dm' = 1)
+    session$setInputs('admin-dz_delete_dm_string' = "delete_data")
+    session$setInputs('admin-dz_delete_dm_items' = TRUE)
+    session$setInputs('admin-dz_delete_dm_confirm' = 1)
+
+    # - tests
+    expect_null(k_data_model())
+    expect_null(k_items())
+    expect_true(file.exists(dm_url))
+    expect_true(file.exists(items_url))
+
+  })
+
+})
+
+
+# -- Cleanup
+clean_all(testdata_path)
