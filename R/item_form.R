3de2f37152d51421c58db9f8a91bd46a3e11e5a8
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-10-22 11:25:26 +0200

    Implement attribute_input #295

--- R/item_form.R
+++ R/item_form.R
@@ -1,11 +1,11 @@
 
 
-#' Build item input tagList
+#' Build Item Form
 #'
-#' @param ns the namespace function, output of shiny::NS()
-#' @param item an optional item (used to set default input values if update = TRUE)
+#' @param data.model the data.frame of the data model
 #' @param update an optional logical (default = FALSE) to trigger update behavior
-#' @param data.model a data.frame containing the data model
+#' @param item an optional item (used to set default input values if update = TRUE)
+#' @param ns the namespace function, output of shiny::NS()
 #'
 #' @return a tagList() object containing the attribute inputs
 #' @export
@@ -15,11 +15,11 @@
 #'
 #' @examples
 #' \dontrun{
-#' item_form(ns, item = NULL, update = FALSE, data.model = mydatamodel)
-#' item_form(ns, item = myitem, update = TRUE, data.model = mydatamodel)
+#' item_form(data.model = mydata$data_model(), update = FALSE, item = NULL, ns)
+#' item_form(data.model = mydata$data_model(), update = TRUE, item = myitem, ns)
 #' }
 
-item_form <- function(ns, item = NULL, update = FALSE, data.model){
+item_form <- function(data.model, update = FALSE, item = NULL, ns){
 
   cat("[item_form] Building input list \n")
   cat("  - update =", update, "\n")
@@ -28,101 +28,6 @@ item_form <- function(ns, item = NULL, update = FALSE, data.model){
   colClasses <- dm_colClasses(data.model)
   skip <- data.model[data.model$skip, ]$name
 
-
-  # -- helper function
-  add_input <- function(x, colClasses, value){
-
-    cat("  - Dealing with attribute :", names(colClasses), "/ type =", colClasses, "/ value =", value, "\n")
-
-    # -- character
-    if(colClasses == "character")
-      input <- textInput(inputId = ns(names(colClasses)),
-                         label = names(colClasses),
-                         value = value,
-                         width = NULL,
-                         placeholder = NULL)
-
-    # -- numeric, integer
-    # removed double #218
-    if(colClasses %in% c("numeric", "integer"))
-      input <- numericInput(
-        inputId = ns(names(colClasses)),
-        label = names(colClasses),
-        value = value,
-        min = NA,
-        max = NA,
-        step = NA,
-        width = NULL)
-
-    # -- date, POSIXct
-    if(colClasses %in% c("Date", "POSIXct"))
-      input <- dateInput(
-        inputId = ns(names(colClasses)),
-        label = names(colClasses),
-        value = value,
-        min = NULL,
-        max = NULL,
-        format = "yyyy-mm-dd",
-        startview = "month",
-        weekstart = 0,
-        language = "en",
-        width = NULL,
-        autoclose = TRUE,
-        datesdisabled = NULL,
-        daysofweekdisabled = NULL)
-
-    # -- POSIXct (add time & timezone inputs)
-    if(colClasses == "POSIXct"){
-
-      # -- get timezone from value
-      # Note: pick values matching with OlsonNames list
-      # set default to Sys.timezone otherwise it will pick first choice
-      tz_value <- attr(as.POSIXlt(value),"tzone")
-      tz_value <- tz_value[tz_value %in% OlsonNames()]
-      if(length(tz_value > 1))
-        tz_value <- utils::head(tz_value, 1)
-      if(length(tz_value == 0))
-        tz_value <- Sys.timezone()
-
-      # -- concatenate with date input
-      input <- wellPanel(input,
-
-                         # -- time (need to extract time from value)
-                         timeInput(inputId = ns(paste0(names(colClasses), "_time")),
-                                   label = paste(names(colClasses), "time"),
-                                   value = strftime(value, format="%H:%M:%S")),
-
-                         # -- timezone (need to extract tz from value)
-                         selectizeInput(inputId = ns(paste0(names(colClasses), "_tz")),
-                                        label = paste(names(colClasses), "timezone"),
-                                        choices = OlsonNames(),
-                                        selected = tz_value))}
-
-
-    # -- logical
-    if(colClasses == "logical"){
-
-      # -- check value
-      if(is.character(value))
-        value <- as.logical(value)
-
-      # -- check NA (in case no default has been set) #246
-      if(is.na(value))
-        value <- FALSE
-
-      # -- input
-      input <- checkboxInput(inputId = ns(names(colClasses)),
-                             label = names(colClasses),
-                             value = value,
-                             width = NULL)
-    }
-
-    # -- return
-    input
-
-  }
-
-
   # -- Filter out attributes in skip param
   cat("  - Filter out attributes to skip:", skip, "\n")
   colClasses <- colClasses[!names(colClasses) %in% skip]
@@ -130,12 +35,12 @@ item_form <- function(ns, item = NULL, update = FALSE, data.model){
   # -- check
   # when id is the only attribute, colClasses will be empty #243
   if(length(colClasses) == 0)
-    return("Warning: there is no attribute that requires an input value (all attributes are skipped!).")
+    return("There is no attribute that requires an input value (all attributes are skipped!).")
 
   # -- Define default input values
   if(update){
 
-    # -- Apply same filter on item to update
+    # -- Apply skip on item to update
     values <- item[names(colClasses)]
 
   } else {
@@ -148,7 +53,7 @@ item_form <- function(ns, item = NULL, update = FALSE, data.model){
 
   # -- apply helper
   cat("[item_form] Loop on attributes: \n")
-  feedback <- lapply(1:length(colClasses), function(x) add_input(x, colClasses[x], values[[x]]))
+  feedback <- lapply(1:length(colClasses), function(x) attribute_input(colClasses[x], values[[x]], ns))
 
   # -- output
   tagList(feedback)
