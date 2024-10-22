d911ad3f0b818b74d22e477d5342420060caaa68
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-10-22 18:15:56 +0200

    Implement shortcuts #295

--- R/item_form.R
+++ R/item_form.R
@@ -3,8 +3,10 @@
 #' Build Item Form
 #'
 #' @param data.model the data.frame of the data model
+#' @param items the data.frame of the items
 #' @param update an optional logical (default = FALSE) to trigger update behavior
 #' @param item an optional item (used to set default input values if update = TRUE)
+#' @param shortcut a logical to indicate if shortcuts should be displayed
 #' @param ns the namespace function, output of shiny::NS()
 #'
 #' @return a tagList() object containing the attribute inputs
@@ -19,7 +21,7 @@
 #' item_form(data.model = mydata$data_model(), update = TRUE, item = myitem, ns)
 #' }
 
-item_form <- function(data.model, update = FALSE, item = NULL, ns){
+item_form <- function(data.model, items, update = FALSE, item = NULL, shortcut = FALSE, ns){
 
   cat("[item_form] Building input list \n")
   cat("  - update =", update, "\n")
@@ -51,11 +53,23 @@ item_form <- function(data.model, update = FALSE, item = NULL, ns){
 
   }
 
-  # -- apply helper
-  cat("[item_form] Loop on attributes: \n")
+  # -- apply attribute_input
+  cat("[item_form] Build attribute input: \n")
   feedback <- lapply(1:length(colClasses), function(x) attribute_input(colClasses[x], values[[x]], ns))
 
+  # -- apply attribute_shortcut
+  if(shortcut){
+    cat("[item_form] Build attribute shortcuts: \n")
+    shortcuts <- lapply(1:length(colClasses), function(x)
+      attribute_shortcut(colClass = colClasses[x],
+                         suggestions = attribute_suggestion(values = items[, names(colClasses[x])]),
+                         ns))}
+
   # -- output
-  tagList(feedback)
+  if(shortcut)
+    tagList(lapply(1:length(colClasses), function(x)
+      div(feedback[x], shortcuts[x], style = 'margin-bottom: 10px;')))
+  else
+    feedback
 
 }
