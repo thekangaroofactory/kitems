5b5b4579daf2823c101bd442d856ab0b4e573a08
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-10-25 10:00:58 +0200

    Fixed #330 #345

--- R/item_add.R
+++ R/item_add.R
@@ -17,6 +17,25 @@ item_add <- function(items, item){
   # -- check items
   stopifnot("reactiveVal" %in% class(items))
 
+  # -- check id uniqueness #330
+  if(item$id %in% items$id)
+    stop("Can't add item to the items data.frame \n id is not unique")
+
+  # -- check item structure #345
+  ifelse(
+    ncol(item) != ncol(items()),
+    stop("Can't add item to the items data.frame \n different column numbers"),
+
+    # -- check column names
+    ifelse(
+      names(item) != names(items()),
+      stop("Can't add item to the items data.frame \n different column names"),
+
+      # -- check column classes
+      if(class(item) != class(items()))
+        stop("Can't add item to the items data.frame \n different column types")))
+
+
   # -- rbind & store
   items(rbind(items(), item))
 
