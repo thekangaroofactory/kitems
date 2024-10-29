d00da75dbf8be564e1e1509a6837921ec0b50092
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2024-10-29 16:39:03 +0100

    Support multiple demos #371

--- R/runExample.R
+++ R/runExample.R
@@ -10,14 +10,25 @@
 #' runExample()
 #' }
 
-runExample <- function() {
+runExample <- function(example = NA) {
+
+  # -- check argument
+  if(is.na(example)){
+
+    # -- get list of apps
+    appDir <- system.file("shiny-examples", package = "kitems")
+    apps <- list.dirs(appDir, recursive = F, full.names = F)
+
+    cat("Available examples:\n")
+    return(apps)}
+
 
   # -- get app path
-  appDir <- system.file("shiny-examples", "demo", package = "kitems")
+  appDir <- system.file("shiny-examples", example, package = "kitems")
 
   # -- check
   if (appDir == "")
-    stop("Could not find example directory. Try re-installing `kitems`.", call. = FALSE)
+    stop("Could not find example directory. Check available examples with runExample().", call. = FALSE)
 
   # -- for testing purpose
   if(require(testthat, quietly = T))
