cabd7ff936d04d2234c3d132e3d9a9146ad22c4c
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-05-14 10:00:16 +0200

    Fixed #409

--- R/item_load.R
+++ R/item_load.R
@@ -29,20 +29,6 @@ item_load <- function(data.model, file = NULL, path = NULL){
 
 
   # ----------------------------------------------------------------------------
-  # Ensure timezone continuity #269
-  # ----------------------------------------------------------------------------
-  # datetime will be read as character (and converted later)
-
-  # # -- get datetime index & name
-  # idx_ct <- which(col.classes %in% "POSIXct")
-  # names_ct <- names(col.classes[idx_ct])
-  #
-  # # -- convert classes
-  # if(length(idx_ct) > 0)
-  #   col.classes[idx_ct] <- "character"
-
-
-  # ----------------------------------------------------------------------------
   # Connector: file (.csv)
   # ----------------------------------------------------------------------------
   if(!is.null(file))
@@ -56,33 +42,9 @@ item_load <- function(data.model, file = NULL, path = NULL){
 
   # ----------------------------------------------------------------------------
 
-
   # -- check output size (will trigger showing the create data btn)
   if(all(dim(items) == c(0,0)))
     items <- NULL
-
-  # else {
-  #
-  #   # --------------------------------------------------------------------------
-  #   # Ensure timezone continuity #269
-  #   # --------------------------------------------------------------------------
-  #   # convert ISO 8601 character vector to POSIXct
-  #   # output will get an extra tzone attribute compared to original object,
-  #   # but values are the same
-  #
-  #   # -- POSIXct
-  #   # check if items has any of the expected attributes #326 #359
-  #   if(length(idx_ct) > 0)
-  #     if(any(names_ct %in% names(items))){
-  #
-  #       # -- because any is used, clean potential missing attribute(s)
-  #       names_ct <- names_ct[names_ct %in% names(items)]
-  #
-  #       catl("[item_load] Converting attribute(s) to POSIXct =", names_ct)
-  #       items[names_ct] <- lapply(items[names_ct], function(x) as.POSIXct(x, format = "%Y-%m-%dT%H:%M:%S%z", tz = ""))}
-  #
-  # }
-
 
   # -- return
   items
