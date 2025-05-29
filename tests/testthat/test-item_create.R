ca5e4d3caa42199e5c4adaebba5189dc362f5689
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-05-29 15:41:53 +0200

    Add test coverage #428

--- tests/testthat/test-item_create.R
+++ tests/testthat/test-item_create.R
@@ -37,3 +37,26 @@ test_that("item_create works with NULL value", {
   expect_false(x$isvalid)
 
 })
+
+
+# -- adding coverage #428
+test_that("item_create works with length(0) value", {
+
+  # -- function call
+  x <- item_create(values = list("id" = c(170539948621),
+                                 "date" = as.Date(numeric(0)),
+                                 "name" = NULL,
+                                 "quantity" = 4,
+                                 "total" = "78.9",
+                                 "isvalid" = c(FALSE)), data.model = dm)
+
+  # -- test class
+  expect_s3_class(x, "data.frame")
+
+  # -- test dim
+  expect_equal(dim(x), c(1, length(colClasses)))
+
+  # -- test logical: #176
+  expect_false(x$isvalid)
+
+})
