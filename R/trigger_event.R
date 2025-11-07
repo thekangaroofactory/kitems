0f4999b311dbc11b90c669f18ce5a5f3cbabb830
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-11-07 11:42:31 +0100

    Fixed #506

---
+++ R/trigger_event.R
@@ -0,0 +1,41 @@
+
+
+#' Create Event
+#'
+#' @param workflow the name of the workflow
+#' @param type the name of the action to be performed
+#' @param values optional values to create, update or delete an item
+#'
+#' @returns a event (list)
+#' @export
+#'
+#' @description
+#' Helper function to create events to be passed to kitems module server
+#' with the trigger argument.
+#'
+#' @examples
+#' \dontrun{
+#' # fire create dialog event
+#' trigger_event()
+#' }
+
+trigger_event <- function(workflow = c("create", "update", "delete"), type = c("dialog", "task"), values = NULL){
+
+  # -- argument checks
+  workflow <- match.arg(workflow)
+  type <- match.arg(type)
+
+  # -- check for when values should not be null
+  if(is.null(values))
+    if(!(workflow == "create" && type == "dialog"))
+      stop("values should be passed for this type of event")
+
+  # -- build default event
+  event <- list(
+    workflow = workflow,
+    type = type)
+
+  # return
+  if(is.null(values)) event else append(event, list(values = values))
+
+}
