a39d13a6165d1828d60821d9493ca4d2a03052e6
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-11-24 21:26:33 +0100

    Fixed #574

--- inst/shiny-examples/demo/server.R
+++ inst/shiny-examples/demo/server.R
@@ -8,28 +8,16 @@
 shinyServer(
   function(input, output, session){
 
-
     # -- Get path to demo app data
     demo_dir <- system.file("shiny-examples", "demo", "data", package = "kitems")
 
-
     # -- Launch module servers ----
     # autosave = FALSE to keep demo data frozen
-    data_2 <- kitems::kitems(id = "data_2", path = demo_dir,
-                             autosave = FALSE, admin = FALSE,
+    data_2 <- kitems::kitems(id = "data_2",
+                             path = demo_dir,
+                             autosave = FALSE,
+                             admin = FALSE,
                              options = list(shortcut = TRUE))
-
-
-    # -- log
-    ktools::catl("----------------------------------------------------------", debug = 1)
-    ktools::catl("Main application server ready", debug = 1)
-    ktools::catl("----------------------------------------------------------", debug = 1)
-
-
-    # -- Observe item lists ----
-    observeEvent(data_2$items(),
-                 ktools::catl("Main application server: data_2 items have just been updated"))
-
 
   }
 )
--- inst/shiny-examples/demo/ui.R
+++ inst/shiny-examples/demo/ui.R
@@ -12,7 +12,8 @@ if(!"shinydashboard" %in% (.packages()))
 # -- Define Sidebar UI ----
 sidebar <- dashboardSidebar(collapsed = FALSE,
                             sidebarMenu(
-                              menuItem("Home", tabName = "home", icon = icon("dashboard"), selected = TRUE)))
+                              menuItem("Introduction", tabName = "intro", icon = icon("dashboard"), selected = TRUE),
+                              menuItem("Content", tabName = "content", icon = icon("dashboard"))))
 
 
 # -- Define Body UI ----
@@ -22,18 +23,22 @@ body <- dashboardBody(
   tabItems(
 
     # -- Content tab
-    tabItem(tabName = "home",
+    tabItem(tabName = "intro",
 
-            # -- Intro
+            # -- intro
             fluidRow(
               column(width = 8,
 
                      h1("Introduction"),
-                     p("This application demonstrates how to implement {kitems} module."),
+                     p("This application demonstrates how to implement {kitems} Shiny module."),
 
-                     h3("Data"),
+                     h2("Implementation"),
+                     p("This demo is based on the 'Full delegation' implementation, in which the item management
+                       is only performed in the kitems() module server (see vignette(\"implementation\"))."),
+
+                     h2("Data"),
                      p("This instance is started with a path that contains a data model file", br(),
-                       "as well as an item file, so it can be displayed in the section below."),
+                       "as well as an item file with some content."),
 
                      # -- warning
                      fluidRow(
@@ -41,60 +46,68 @@ body <- dashboardBody(
                            width = 12,
                            solidHeader = TRUE,
                            status = "info",
+                           "The autosave argument of the module server function is set to FALSE for demonstration purpose.",
                            "Create / update / delete won't be persistent after the app is closed.")),
 
                      h3("Admin console"),
-                     p("Administration console is a dedicated app.", br(),
-                       "To access it, run the admin() function"),
-                     tags$pre("# -- start admin console: \nadmin(system.file(\"shiny-examples\", \"demo\", \"data\", package = \"kitems\"))"))),
+                     p("Administration console is a dedicated Shiny app.", br(),
+                       "To access it, run the admin() function:"),
+                     tags$pre("# -- start admin console \nadmin(system.file(\"shiny-examples\", \"demo\", \"data\", package = \"kitems\"))"))),
 
 
-            # -- data_2
+            ),
+
+    # -- Content tab
+    tabItem(tabName = "content",
+
+            # -- components
             fluidRow(
-              column(width = 8,
+              column(width = 6,
 
                      h1("Components"),
 
-                     h3("Date sliderInput"),
-                     p("The date sliderInput UI component is available whenever the data model has an attribute named \'date\'", br(),
-                       tags$pre("# -- Date filter: \ndate_slider_widget(id)"),
+                     h2("Action buttons"),
+                     p("Create, update and delete action buttons are provided.", br(),
+                       "Their visibility is contextual to the item table selection."),
+
+                     kitems::create_widget("data_2"),
+                     kitems::update_widget("data_2"),
+                     kitems::delete_widget("data_2"), p(),
+
+                     tags$pre("# -- Create button: \ncreate_widget(id) \n# -- Update button: \nupdate_widget(id) \n# -- Dete button: \ndelete_widget(id)"),
+
+                     h2("Date slider input"),
+                     p("The date slider component is available whenever the data model has an attribute named \'date\' (strictly)", br(),
                        "It is initialized with the values from the items table.", br(),
-                       "Selected range will be applied on the filtered data table."),
+                       "Selected range will be applied on the filtered view."),
+                     p("The strategies are sescribed in the filtering documentation article."),
 
                      wellPanel(
                        fluidRow(column(width = 1),
                                 column(width = 11,
                                        kitems::date_slider_widget("data_2")))),
 
-                     p("Strategies:"),
-                     tags$ul(
-                       tags$li("this-year: default range will be limited to dates belonging to the current year.", br(),
-                               "when an item is added, selection will be extended to include the new date if it fits with current year."),
-                       tags$li("keep-range: the range will stick to what user has previously selected.")),
+                     tags$pre("# -- Date filter: \ndate_slider_widget(id)")),
 
+              column(width = 6,
 
-                     h3("Action buttons"),
-
-                     p("Create, update and delete action buttons are provided."),
-                     p("Their UI functions will either return a button or NULL:"),
-                     tags$ul(
-                       tags$li("create will be NULL if the data model is empty"),
-                       tags$li("update & delete will be NULL if no row is selected in the table")),
-
-                     kitems::create_widget("data_2"),
-                     kitems::update_widget("data_2"),
-                     kitems::delete_widget("data_2"),
-                     p(""),
-
-
-                     h3("Items table"),
-
+                     h2("Items table"),
                      p("A (filtered) item view is available."),
-                     tags$pre("# -- Filtered view: \nfiltered_view_widget(id)"),
 
                      fluidRow(column(width = 12,
                                      wellPanel(
-                                       kitems::filtered_view_widget("data_2")))))))
+                                       kitems::filtered_view_widget("data_2")))),
+
+
+                     tags$pre("# -- Filtered view: \nfiltered_view_widget(id)"),
+
+                     h2("Server side"),
+                     p("The server function of this demo app contains a single expression!"),
+
+                     tags$pre("# -- launch kitems module server \ndata_2 <- kitems::kitems(id = \"data_2\", \npath = demo_dir, \nautosave = FALSE, \nadmin = FALSE, \noptions = list(shortcut = TRUE))")
+
+
+                     )))
 
 
   ) # end tabItems
