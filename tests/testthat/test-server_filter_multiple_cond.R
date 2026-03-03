97b9a0ba67759a2d4e6f02621d3d4e3e0ce4e0c6
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-03-03 14:30:49 +0100

    Update test coverage #601

--- tests/testthat/test-server_filter_multiple_cond.R
+++ tests/testthat/test-server_filter_multiple_cond.R
@@ -33,6 +33,32 @@ test_that("Filter with multiple conditions works", {
 
 
 # --------------------------------------------------------------------------
+# Scenario:
+# --------------------------------------------------------------------------
+
+test_that("Filter with multiple conditions works", {
+
+  # -- declare arguments
+  params <- list(id = module_id,
+                 path = testdata_path,
+                 autosave = TRUE,
+                 filter = reactiveVal(kitems::filter_event(layer = "main", name == "Banana", isvalid == FALSE)))
+
+  # -- module server call
+  testServer(kitems, args = params, {
+
+    # -- flush
+    session$flushReact()
+
+    # -- checks
+    expect_true(all(filtered_items()$name == "Banana"))
+    expect_true(all(filtered_items()$isvalid == FALSE))
+
+  })
+
+})
+
+# --------------------------------------------------------------------------
 # Cleanup
 # --------------------------------------------------------------------------
 
