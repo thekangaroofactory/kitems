cbfbcc82df9975cf7afea8f0ef85624ec2c294a7
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-03-09 16:07:43 +0100

    Fixed #425

--- R/admin.R
+++ R/admin.R
@@ -5,17 +5,6 @@
 #' @description
 #' Launches the administration console (Shiny App)
 #'
-#' @param path where to find the data model & item folder(s)
-#'
-#' @details
-#' The recommended way to define the `path` argument is to set the R_KITEMS_PATH
-#' environment variable.
-#'
-#' The app will scan `path` to detect sub folders that are expected to be
-#' item folders named after the id used to create them.
-#'
-#' It will build the ui tabs from this list.
-#'
 #' @export
 #'
 #' @examples
@@ -23,18 +12,14 @@
 #' admin()
 #' }
 
-# -- function definition
-admin <- function(path = Sys.getenv("R_KITEMS_PATH")) {
+admin <- function() {
 
   # -- get app path
   appDir <- system.file("shiny", package = "kitems")
 
   # -- check30
   if(appDir == "")
-    stop("Could not find shiny directory. Try re-installing `kitems`.", call. = FALSE)
-
-  # -- set option (the app will get it)
-  shiny::shinyOptions(kitems_path = path)
+    stop("Could not find shiny folder. Try re-installing `kitems`.", call. = FALSE)
 
   # -- run app
   shiny::runApp(file.path(appDir, "admin_console.R"), display.mode = "normal")
