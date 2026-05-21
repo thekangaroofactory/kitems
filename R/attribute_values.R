70408cda1c94f70bf19e153944719b9cd6c4b585
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-05-21 18:51:33 +0200

    Fixed #663

--- R/attribute_values.R
+++ R/attribute_values.R
@@ -1,62 +1,88 @@
 
 
-#' Turn Value(s) into Attribute
+#' Validate Value(s)
 #'
-#' @param key the name of the attribute.
-#' @param value a vector of input values.
+#' @description
+#' Validate the input values and turn them into item(s)
+#'
+#' @param value a list of input values.
 #' @param data.model the data.frame of the data model.
+#' @param update whether the id attribute should be checked or not (default FALSE).
 #'
-#' @returns A vector of values checked against the data model.
+#' @returns A data.frame of item(s) checked against the data model.
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' attribute_values(key, value, data.model)}
+#' attribute_values(values, data.model)}
 
-attribute_values <- function(key, value, data.model){
+attribute_values <- function(values, data.model, update = FALSE){
 
-  # -- extract expected class from data.model
-  colClass <- dm_colClasses(data.model)[key]
-
-  # -- input summary (debug)
-  catl("Process attribute: key =", key)
-  catl("- input value =", as.character(value), level = 2)
-  catl("- target class =", colClass, level = 2)
-
+  # the input for this function will be the named values instead of key/value
+  # - extract from the input
+  # - prepared from the trigger (squared)
+  # >> done
 
   # ////////////////////////////////////////////////////////////////////////////
-  # -- Validate or replace input values
+  # -- helper function
+  # dealing with a single attribute
 
-  # -- depends on single / multiple values
-  if(length(value) <= 1){
+  helper <- function(key, value, att_dm){
 
-    if(!is_truthy(value)){
-      catl("> Attribute has invalid input / set default", level = 2)
-      value <- dm_default(data.model |> dplyr::filter(name == key))$default}
+    # -- input summary (debug)
+    catl("Process attribute: key =", key)
+    catl("- input value =", as.character(value), level = 2)
 
-  } else {
+    # //////////////////////////////////////////////////////////////////////////
+    # -- Validate or replace value(s)
 
-    # -- get valid values
-    is_valid <- sapply(value, is_truthy)
+    # -- depends on single / multiple values
+    if(length(value) <= 1){
 
-    # -- replace invalid ones by default
-    if(any(!is_valid)){
-      catl("> Attribute has invalid input(s) / set default", level = 2)
-      value[!is_valid] <- dm_default(data.model, key, n = length(value[!is_valid]))}
+      if(!is_truthy(value)){
+        catl("> invalid input / set default", level = 2)
+        value <- dm_default(att_dm)$default}
+
+    } else {
+
+      # -- get valid values
+      is_valid <- sapply(value, is_truthy)
+
+      # -- replace invalid ones by default
+      if(any(!is_valid)){
+        catl("> invalid input(s) / set default where needed", level = 2)
+        value[!is_valid] <- replicate(dm_default(att_dm)$default, n = length(value[!is_valid]))}
+
+    }
+
+    # //////////////////////////////////////////////////////////////////////////
+    # -- check vs target class
+    # value might have several classes (case POSIX*)
+
+    # -- input summary (debug)
+    catl("- target class =", att_dm$type, level = 2)
+
+    if(!att_dm$type %in% class(value)){
+      catl("> Class:", class(value), "does not fit with:", att_dm$type, level = 2)
+      value <- convert(value, att_dm$type, att_dm$class.arg)}
+
+    # -- output summary (debug)
+    catl(">> Output: class =", class(value), "/ value =", as.character(value))
+
+    # -- return
+    value
 
   }
 
+
   # ////////////////////////////////////////////////////////////////////////////
-  # -- check vs target class
-  # value might have several classes (case POSIX*)
-  if(!colClass %in% class(value)){
-    catl("> Class:", class(value), "does not fit with:", colClass, "/ Coerce to target class", level = 2)
-    value <- convert(value, colClass, data.model[data.model$name == key, ]$class.arg)}
+  # -- check values & types
 
-  # -- output summary (debug)
-  catl("> Output: class =", class(value), "/ value =", as.character(value))
-
-  # -- return
-  value
+  sapply(names(values),
+         function(x) helper(key = x,
+                            value = values[[x]],
+                            att_dm = data.model[data.model$name == x, ]),
+         simplify = FALSE,
+         USE.NAMES = TRUE)
 
 }
