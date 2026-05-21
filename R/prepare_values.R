84bd038e36f9dc41e316fad3a1c3c4296567b2f5
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-05-21 18:50:17 +0200

    Fixed #662

---
+++ R/prepare_values.R
@@ -0,0 +1,64 @@
+
+
+#' Prepare Values
+#'
+#' @description
+#' Turn trigger input values into tabular data
+#'
+#' @param values a list of values.
+#' @param data.model the data.frame of the data model.
+#' @param update a logical if values are used in the update workflow (default FALSE).
+#'
+#' @returns a data.frame of values
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' prepare_values(values, data.model)
+#' }
+
+prepare_values <- function(values, data.model, update = FALSE){
+
+  # ////////////////////////////////////////////////////////////////////////////
+  # -- cleanup & prepare values
+
+  # -- drop unmatched columns
+  # before projection to avoid potential duplicated rows
+  values <- values[names(values) %in% data.model$name]
+
+  # -- secure against length 0 (NULL, numeric(0)...)
+  # otherwise as.data.frame will fail
+  if(any(lengths(values) == 0))
+    values <- values[lengths(values) != 0]
+
+  # -- make rectangular
+  # elements must have length 1 or same as the id element
+  values <- as.data.frame(values)
+
+
+  # ////////////////////////////////////////////////////////////////////////////
+  # -- Specific to create
+
+  # -- secure against missing columns
+  if(!update)
+    if(any(!data.model$name %in% names(values))){
+      catl("- Adding missing columns", level = 2)
+      values[data.model$name[!data.model$name %in% names(values)]] <- NA
+      values <- values[data.model$name]}
+
+
+  # ////////////////////////////////////////////////////////////////////////////
+  # -- Specific to update
+
+  # -- check skipped attributes to refresh
+  # add names to values so they will be computed again
+  if(update){
+    att_refresh <- data.model |> dplyr::filter(.data$skip, .data$refresh) |> dplyr::pull(.data$name)
+    att_refresh <- att_refresh[!att_refresh %in% names(values)]
+    if(!identical(att_refresh, character(0)))
+      values <- c(values, as.list(stats::setNames(NA, att_refresh)))}
+
+  # -- return
+  values
+
+}
