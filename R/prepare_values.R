434c0c18a7c09f74ccc2ca8679749928018f9caf
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-05-21 18:58:54 +0200

    Doc #662

--- R/prepare_values.R
+++ R/prepare_values.R
@@ -9,6 +9,16 @@
 #' @param data.model the data.frame of the data model.
 #' @param update a logical if values are used in the update workflow (default FALSE).
 #'
+#' @details
+#' values is a named list. The names are used to check the corresponding values
+#' vs the data.model (class, default values if the provided ones are not valid).
+#' The elements in the list must have either length one or same length as the id element.
+#'
+#' When an element has length one but the id has several values, all items corresponding
+#' to these ids will be updated with same value. To do so, values will be turned into
+#' a data.frame using as.data.frame ; for this reason, it's strongly advised to wrap
+#' the call into tryCatch as this may fail.
+#'
 #' @returns a data.frame of values
 #' @export
 #'
