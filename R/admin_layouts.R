1360997b8eb1b870d4c82cb472ccff2b4d6f01bb
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-06-13 13:11:47 +0200

    #699

---
+++ R/admin_layouts.R
@@ -0,0 +1,21 @@
+
+
+admin_no_yaml_layout <- function(){
+
+  div(id = "home-no-yaml",
+      h1("Welcome"),
+      h2("There is no YAML configuration available for this project."),
+      p("Click", actionLink(inputId = "yaml_create", label = "here"), "to create one."))
+
+}
+
+
+admin_yaml_message <- function(config){
+
+  if(is.null(config))
+    return(p(class = "text-warning", icon("circle-chevron-right"), "The project has no config file!"))
+
+  if(is.null(config$items))
+    return(p(class = "text-warning", icon("circle-chevron-right"), "The project has no item yet."))
+
+}
