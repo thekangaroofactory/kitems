95b850ce3a40861f88af8de21511b4e9c5b7b461
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-08-26 08:39:35 +0200

    Implement #771

--- R/extend.R
+++ R/extend.R
@@ -13,15 +13,27 @@
 #' This function is a wrapper around design() to provide a comprehensive
 #' layered syntax.
 #'
+#' It only understands the `attribute` instruction inside `...`:
+#' attribute = c(name = "note", type = "character")
+#' Anything else will be filtered.
+#'
 #' @returns a config list()
 #' @export
 #'
 #' @examples
-#' design(project = "test",
-#         item = "foo") |>
-#         extend(item = "foo",
-#                attribute = c(name = "date", type = "Date", default = "Sys.Date()"))
-
+#' # create config
+#' config <- design(project = "test",
+#'         item = "foo")
+#'
+#' # extend
+#' config |>
+#'   extend(item = "foo",
+#'          attribute = c(name = "date", type = "Date", default = "Sys.Date()"))
+#'
+#' # is same as
+#' config <- design(project = "test",
+#'         item = "foo",
+#'         attribute = c(name = "date", type = "Date", default = "Sys.Date()"))
 
 extend <- function(config, item, ...){
 
