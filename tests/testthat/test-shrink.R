b34fa6369e9b721a91ba851ac0a50a64bf530995
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-08-26 12:19:24 +0200

    Test coverage #773

---
+++ tests/testthat/test-shrink.R
@@ -0,0 +1,44 @@
+
+create_folder()
+
+test_that("Verb shrink works", {
+
+  # baseline
+  config <- design(project = "test",
+                   item = "foo") |>
+    extend(item = "foo",
+           attribute = c(name = "total", type = "integer"))
+
+  # drop item
+  x <- config |>
+    shrink(item = "foo")
+  expect_identical(x$items, list())
+
+  # drop attribute
+  x <- config |>
+    shrink(attribute = c(item = "foo", name = "total"))
+  expect_identical(config_attributes(x, item = "foo"), "id")
+
+  # multiple instructions
+  x <- config |>
+    shrink(attribute = c(item = "foo", name = "total"),
+           item = "foo")
+  expect_identical(x$items, list())
+
+  # missing item
+  expect_warning(x <- config |> shrink(item = "dummy"))
+  expect_identical(x, config)
+
+  # missing attribute
+  expect_warning(x <- config |>
+                   shrink(attribute = c(item = "foo", name = "dummy")))
+  expect_identical(x, config)
+
+  # both missing
+  expect_warning(x <- config |>
+                   shrink(attribute = c(item = "dummy", name = "dummy")))
+  expect_identical(x, config)
+
+})
+
+clean_all()
