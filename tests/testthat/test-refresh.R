c66e302e4c5bce84eae8fa84f8290d0c71ec94c7
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-08-27 19:25:18 +0200

    Test coverage #785

---
+++ tests/testthat/test-refresh.R
@@ -0,0 +1,22 @@
+
+create_folder()
+
+test_that("Verb refresh works", {
+
+  # baseline
+  config <- design(project = "test", item = "foo") |>
+    extend(item = "foo",
+           attribute = c(name = "total", type = "integer")) |>
+    skip(item = "foo", "total")
+
+  # -- refresh & refreshed
+  x <- config |> refresh(item = "foo", "total")
+  expect_identical(x |> refreshed(item = "foo"), "total")
+
+  # -- freeze & frozen
+  x <- config |> freeze(item = "foo", "total")
+  expect_identical(x |> frozen(item = "foo"), c("id", "total"))
+
+})
+
+clean_all()
