97d6ff785bd493a8294f6b6199b9104a8fc15e26
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-08-28 08:51:02 +0200

    Test coverage #787

---
+++ tests/testthat/test-hide.R
@@ -0,0 +1,28 @@
+
+create_folder()
+
+test_that("Grammar hide works", {
+
+  # baseline
+  config <- design(project = "test",
+                   item = "foo") |>
+    extend(item = "foo",
+           attribute = c(name = "date", type = "Date"))
+
+  # hide attribute
+  expect_no_warning(config <- config |>
+                      hide(item = "foo", "date"))
+
+  # hidden attributes
+  expect_identical(config |> hidden(item = "foo"), c("id", "date"))
+  #'
+  # display attribute
+  expect_no_warning(config <- config |>
+                      display(item = "foo", "date"))
+  #'
+  # displayed attributes
+  expect_identical(config |> displayed(item = "foo"), "date")
+
+})
+
+clean_all()
