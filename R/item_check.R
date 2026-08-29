1cb1aa3d91b6cbb6bbca47c7e03c3668e33d2637
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-08-29 12:16:08 +0200

    Fixed #793

---
+++ R/item_check.R
@@ -0,0 +1,64 @@
+
+
+#' Check Items
+#'
+#' @param items the data.frame of the items
+#' @param config the YAML config list
+#' @param id the name of the item
+#'
+#' @details
+#' It is expected that config is checked first using [config_check()].
+#'
+#' @returns a list
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' item_check(items, config, "foo")
+#'}
+
+item_check <- function(items, config, id){
+
+  # -- check args (basic)
+  stopifnot("items must be a data.frame" = is.data.frame(items))
+  stopifnot("config must be a list" = is.list(items))
+
+  rc <- list()
+
+  # -- columns
+  if(!identical(names(items), config_attributes(config, id))){
+
+    # columns in data.model
+    if(!all(x <- names(items) %in% config_attributes(config, id)))
+      rc <- c(rc, list(code = 1, type = "error",
+                       message = paste("Column(s)", paste(names(items)[x], collapse = ", "), "not in the data.model"),
+                       missing = names(items)[x]))
+
+    # missing columns
+    if(any(x <- !config_attributes(config, id) %in% names(items)))
+      rc <- c(rc, list(code = 2, type = "error",
+                       message = paste("Attribute(s)", paste(config_attributes(config, id)[x], collapse = ", "), "not in the items"),
+                       missing = config_attributes(config, id)[x]))
+
+  }
+
+  # -- types
+
+  # items classes
+  # when POSIXct, two classes will be found
+  if(is.list(items_classes <- sapply(items, class)))
+    items_classes <- sapply(items_classes, "[[", 1)
+
+  if(!identical(items_classes, config_item_colclasses(config, id))){
+    x <- names(items_classes[which(items_classes != config_item_colclasses(config, id))])
+    if(length(x))
+      rc <- c(rc, list(code = 3, type = "error",
+                       message = paste("Column(s)", paste(x, collapse = ", "), "type not matching with the data.model")))
+
+  }
+
+  # -- return
+  rc
+
+}
+
