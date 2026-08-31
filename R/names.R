800289394a6292ae242deabc58dce1599f4fcce1
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-08-31 17:44:00 +0200

    names #789

--- R/names.R
+++ R/names.R
@@ -1,23 +1,9 @@
 
 
-#' Data Model Name
+#' Data Model Name & URL
 #'
 #' @description
-#' Helper function to compute the data model name.
-#'
-#'
-#' @param id the id of the module server instance.
-#'
-#' @return The name of the corresponding data model.
-#' @export
-#'
-#' @examples
-#' dm_name(id = "mydata")
-
-dm_name <- function(id){paste0(id, "_data_model")}
-
-
-#' Data Model URL
+#' Helper functions to compute the data model name and URL.
 #'
 #' @param id the id of the module server instance.
 #' @param path the path to the data model and items.
@@ -26,11 +12,20 @@ dm_name <- function(id){paste0(id, "_data_model")}
 #' The recommended way to define the `path` argument is to set the R_KITEMS_PATH
 #' environment variable.
 #'
-#' @returns The URL of the data model
+#' These functions are kept for backward compatibility reasons.
+#' (backup / restore before migration).
+#'
+#' @return The name or url of the data model.
 #' @export
 #'
 #' @examples
+#' dm_name(id = "mydata")
 #' dm_url(id = "mydata")
+
+dm_name <- function(id){paste0(id, "_data_model")}
+
+#' @rdname refresh
+#' @export
 
 dm_url <- function(id, path = Sys.getenv("R_KITEMS_PATH")){file.path(path, paste0(dm_name(id), ".rds"))}
 
