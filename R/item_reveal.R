3e3bb5e14aad8586e7a2cc53cf34945418eeac36
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-09-01 15:13:44 +0200

    item_reveal #795

--- R/item_reveal.R
+++ R/item_reveal.R
@@ -7,23 +7,27 @@
 #'
 #' @param items the data.frame of the items.
 #' @param config the config list.
-#' @param item the name (id) of the item group.
+#' @param item optional (see details), the name (id) of the item group.
 #'
 #' @details
 #' The data model display mask is defined at the data.model level.
 #' use [hide()] or [display()] to tune it.
 #'
-#' @seealso [hide()][display()]
+#' When `item` is set in the parent frame, then the attribute can be skipped
+#' in the function call.
+#'
+#' @seealso [hide()][display()][parent.frame()]
 #'
 #' @return A data.frame.
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' item_reveal(items, config, item = "foo")
+#' item <- "foo"
+#' items |> item_reveal(config)
 #' }
 
-item_reveal <- function(items, config, item = NULL){
+item_reveal <- function(items, config, item = get_context()){
 
   items[displayed(config, item)]
 
