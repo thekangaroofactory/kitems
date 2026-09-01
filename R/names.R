f7fa205afa0906d2833cb4927fa3d0b4d69b2557
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-09-01 09:48:19 +0200

    Implement #801

--- R/names.R
+++ R/names.R
@@ -1,64 +1,61 @@
 
 
-#' Data Model Name & URL
+#' Kitems Names
 #'
 #' @description
-#' Helper functions to compute the data model name and URL.
+#' Helper function to homogenize names across the package.
 #'
-#' @param id the id of the module server instance.
-#' @param path the path to the data model and items.
-#'
-#' @details
-#' The recommended way to define the `path` argument is to set the R_KITEMS_PATH
-#' environment variable.
-#'
-#' These functions are kept for backward compatibility reasons.
-#' (backup / restore before migration).
-#'
-#' @return The name or url of the data model.
-#' @export
-#'
-#' @examples
-#' dm_name(id = "mydata")
-#' dm_url(id = "mydata")
-
-dm_name <- function(id){paste0(id, "_data_model")}
-
-#' @rdname refresh
-#' @export
-
-dm_url <- function(id, path = Sys.getenv("R_KITEMS_PATH")){file.path(path, paste0(dm_name(id), ".rds"))}
-
-
-#' Items Name
-#'
-#' @description
-#' Helper function to compute the items name.
-#'
-#' @param id the id of the module server instance
-#'
-#' @return The name of the corresponding items.
-#' @export
-#'
-#' @examples
-#' items_name(id = "mydata")
-
-items_name <- function(id){paste0(id, "_items")}
-
-
-#' Items URL
-#'
-#' @param id the id of the module server instance.
-#' @param path the path to the data model and items.
+#' @param id the name (id) of the item group.
+#' @param what the targeted object (item, dm or config).
+#' @param file a logical if file extension should be added.
+#' @param url a logical if the url should be returned.
+#' @param backup a logical if a timestamp should be added.
 #'
 #' @details
-#' The recommended way to define the `path` argument is to set the R_KITEMS_PATH
-#' environment variable.
+#' "dm" is kept for backward compatibility reasons (backup & restore old
+#' data.model files before migration to YAML config).
 #'
-#' @returns The URL of the items
+#' When `url` is `TRUE`, then `file` is considered `TRUE` as well.
+#' When `backup` is `TRUE`, then `file` is considered `TRUE` as well.
+#'
+#' Note that from v0.8.0 url for the config is build from the
+#' `R_KITEMS_PATH` environment variable and url for the item groups
+#' is contained in the YAML config.
+#'
+#' @returns a character string
 #' @export
 #'
 #' @examples
-#' items_url(id = "mydata")
+#' # item name
+#' name("foo")
 
-items_url <- function(id, path = Sys.getenv("R_KITEMS_PATH")){file.path(path, paste0(items_name(id), ".csv"))}
+name <- function(id = NULL, what = c("item", "dm", "config"), file = FALSE, url = FALSE, backup = FALSE){
+
+  # -- check
+  what <- match.arg(what)
+
+  # -- name
+  nm <- switch (what,
+                dm = paste0(id, "_data_model"),
+                config = "_kitems",
+                paste0(id, "_items"))
+
+  # -- data.model backup
+  if(backup)
+    nm <- paste(nm, format(Sys.time(), "%Y%m%d_T%H%M%S"), sep = "_")
+
+  # -- filename
+  if(file || url || backup)
+    nm <- switch (what,
+                  dm = paste0(nm, ".rds"),
+                  config = paste0(nm, ".yml"),
+                  paste0(nm, ".csv"))
+
+  # -- url
+  if(url)
+    nm <- file.path(ifelse(what != "config", file.path(Sys.getenv("R_KITEMS_PATH"), id), Sys.getenv("R_KITEMS_PATH")), nm)
+
+  # -- return
+  nm
+
+}
