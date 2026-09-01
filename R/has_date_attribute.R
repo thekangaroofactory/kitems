d5e16d35dbed23e7173101fc18697ea9802f12e9
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-09-01 15:01:23 +0200

    has_date_attribute #795

--- R/has_date_attribute.R
+++ R/has_date_attribute.R
@@ -6,7 +6,13 @@
 #' Basic check if item has a 'date' attribute.
 #'
 #' @param config the config list
-#' @param item the name (id) of the item group
+#' @param item optional (see details), the name (id) of the item group
+#'
+#' @details
+#' When `item` is set in the parent frame, then the attribute can be skipped
+#' in the function call.
+#'
+#' @seealso [parent.frame()]
 #'
 #' @returns a logical
 #' @export
@@ -14,7 +20,7 @@
 #' @examples
 #' has_date(design(project = "test))
 
-has_date_attribute <- function(config, item){
+has_date_attribute <- function(config, item = get_context()){
 
   "date" %in% names(config_item_colclasses(config, item))
 
