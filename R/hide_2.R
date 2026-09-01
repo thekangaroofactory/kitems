2789771fe28c4e12e59c1472a0384f160e22b032
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-09-01 15:04:06 +0200

    Display

--- R/hide_2.R
+++ R/hide_2.R
@@ -5,14 +5,19 @@
 #' @description
 #' A set of verbs to manipulate the display of an attribute.
 #'
-#' @param config the config list
-#' @param item the name (id) of the item group
-#' @param ... the name of the attribute(s) to manipulate
+#' @param config the config list.
+#' @param item optional (see details), the name (id) of the item group.
+#' @param ... the name of the attribute(s) to manipulate.
 #'
 #' @details
 #' `hide()` and `display()` are setter functions to tune attribute's behavior.
 #' `hidden()` and `displayed()` are getter functions to quickly access the attributes
 #' to hide or display in the item table.
+#'
+#' When `item` is set in the parent frame, then the attribute can be skipped
+#' in the function call.
+#'
+#' @seealso [parent.frame()]
 #'
 #' @returns a config list (setters) or a character vector (getters)
 #' @export
@@ -24,29 +29,32 @@
 #' extend(item = "foo", attribute = c(name = "date", type = "Date")) |>
 #' hide(item = "foo", "date")
 #'
+#' # allow skipping argument in following calls
+#' item <- "foo"
+#'
 #' # hidden attributes
-#' config |> hidden(item = "foo")
+#' config |> hidden()
 #'
 #' # display attribute
-#' config |> display(item = "foo", "date")
+#' config |> display("date")
 #'
 #' # displayed attributes
-#' config |> displayed(item = "foo")
+#' config |> displayed()
 
-hide <- function(config, item, ...){
+hide <- function(config, item = get_context(), ...){
   config_attribute_behavior(config, item, behavior = "hide", ...)}
 
 #' @rdname hide
 #' @export
-display <- function(config, item, ...){
+display <- function(config, item = get_context(), ...){
   config_attribute_behavior(config, item, behavior = "hide", ..., set = FALSE)}
 
 #' @rdname hide
 #' @export
-hidden <- function(config, item){
+hidden <- function(config, item = get_context()){
   config_item_behavior(config, item, behavior = "hide")}
 
 #' @rdname hide
 #' @export
-displayed <- function(config, item){
+displayed <- function(config, item = get_context()){
   config_attributes(config, item)[!config_attributes(config, item) %in% config_item_behavior(config, item, behavior = "hide")]}
