7aa81938f74a9fbdc4c673247a060da19620e928
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-09-05 19:47:31 +0200

    date_slider_widget #819

--- tests/testthat/test-date_slider_widget.R
+++ tests/testthat/test-date_slider_widget.R
@@ -5,10 +5,8 @@ test_that("date_slider_widget works", {
   # -- function call
   x <- date_slider_widget(module_id)
 
-  # -- test class
+  # -- tests
   expect_type(x, "list")
-
-  # -- test output
   expect_equal(length(x), 2)
 
 })
