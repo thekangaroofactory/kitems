e855d3df18bd356542d2d8bee95d9aa9bbc352ca
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-09-05 19:19:20 +0200

    Buttons #819

--- tests/testthat/test-buttons_ui.R
+++ tests/testthat/test-buttons_ui.R
@@ -5,11 +5,9 @@ test_that("create_widget works", {
   # -- function call
   x <- create_widget(module_id)
 
-  # -- test class
+  # -- tests
   expect_type(x, "list")
-
-  # -- test output
-  expect_equal(unlist(x), c(name = "span", attribs.id = "data-item_create_btn", attribs.class = "shiny-html-output"))
+  expect_equal(unlist(x), c(name = "span", attribs.id = paste0(module_id, "-item_create_btn"), attribs.class = "shiny-html-output"))
 
 })
 
@@ -19,11 +17,9 @@ test_that("update_widget works", {
   # -- function call
   x <- update_widget(module_id)
 
-  # -- test class
+  # -- tests
   expect_type(x, "list")
-
-  # -- test output
-  expect_equal(unlist(x), c(name = "span", attribs.id = "data-item_update_btn", attribs.class = "shiny-html-output"))
+  expect_equal(unlist(x), c(name = "span", attribs.id = paste0(module_id, "-item_update_btn"), attribs.class = "shiny-html-output"))
 
 })
 
@@ -33,10 +29,8 @@ test_that("delete_widget works", {
   # -- function call
   x <- delete_widget(module_id)
 
-  # -- test class
+  # -- tests
   expect_type(x, "list")
-
-  # -- test output
-  expect_equal(unlist(x), c(name = "span", attribs.id = "data-item_delete_btn", attribs.class = "shiny-html-output"))
+  expect_equal(unlist(x), c(name = "span", attribs.id = paste0(module_id, "-item_delete_btn"), attribs.class = "shiny-html-output"))
 
 })
