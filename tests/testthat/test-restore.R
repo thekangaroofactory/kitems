274397ba0df1e569ea2bad74407562a3dc553e9c
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-09-05 19:27:52 +0200

    restore #821

--- tests/testthat/test-restore.R
+++ tests/testthat/test-restore.R
@@ -2,8 +2,10 @@
 # -- setup
 create_testdata()
 
-# -- test
 test_that("restore data.model works", {
+
+  # -- simulate dm file (dummy content)
+  saveRDS(TRUE, file = name(module_id, what = "dm", url = T))
 
   # -- create backup & restore it
   backup(id = module_id, type = "dm", max = 1)
@@ -11,9 +13,28 @@ test_that("restore data.model works", {
 
   # -- check
   n <- length(list.files(path = testdata_path, pattern = name(module_id, what = "dm")))
-  expect_equal(n, 2)
+  expect_equal(n, 1)
 
 })
+
+
+test_that("restore config works", {
+
+  # -- function call
+  # create first backup
+  backup(id = module_id, type = "config", max = 1)
+  restore(id = module_id, type = "config")
+
+  # -- check
+  url <- file.path(testdata_path, name(module_id, file = T))
+  expect_true(file.exists(url))
+
+  # -- check
+  n <- length(list.files(path = testdata_path, pattern = name(module_id)))
+  expect_equal(n, 1)
+
+})
+
 
 # -- test
 test_that("restore items works", {
@@ -29,7 +50,7 @@ test_that("restore items works", {
 
   # -- check
   n <- length(list.files(path = testdata_path, pattern = name(module_id)))
-  expect_equal(n, 2)
+  expect_equal(n, 1)
 
 })
 
