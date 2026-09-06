d0d41b032da9dcf6e9447ff7d1db9016721129f1
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-09-06 17:42:06 +0200

    Fix test

--- tests/testthat/test-as_default.R
+++ tests/testthat/test-as_default.R
@@ -3,6 +3,6 @@ test_that("as_default works", {
 
   x <- as_default(item = items[2, ], data.model = yaml_to_dm(config, item = "foo", "name", "type", "default", "values"))
   expect_s3_class(x, c("tbl_df", "tbl", "data.frame"))
-  expect_identical(dim(x), c(7, 3))
+  expect_identical(dim(x), c(7L, 3L))
 
 })
