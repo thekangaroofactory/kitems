7192df61f868caa4c1a623dedd2a77694e643464
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-09-07 15:20:20 +0200

    Fixed #733

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -4,7 +4,7 @@
 # Declare shared objects
 # ------------------------------------------------------------------------------
 
-# -- declare namespace
+# -- namespace
 ns <- shiny::NS("id")
 
 # -- module id
@@ -19,33 +19,20 @@ module_id <- "foo"
 ktools::trace_level(0)
 
 # -- data folder
-# create & export env
 testdata_path_base <- file.path(system.file("tests", "testthat", package = "kitems"), "testdata")
 testdata_path <- file.path(testdata_path_base, module_id)
 
-dir.create(testdata_path, recursive = TRUE, showWarnings = FALSE)
+# -- helper
+create_test_folder <- function(x)
+  dir.create(x, recursive = TRUE, showWarnings = FALSE)
+
+# -- create & set env
+create_test_folder(testdata_path)
 Sys.setenv("R_KITEMS_PATH" = testdata_path_base)
 
 # -- urls
 config_url <- name(module_id, what = "config", url = T)
 items_url <- name(module_id, url = T)
-
-
-# ------------------------------------------------------------------------------
-# Declare objects to build data model
-# ------------------------------------------------------------------------------
-
-# most probably:
-#
-# make a few use cases to create test data:
-# - baseline with config & items
-# -
-#
-# Specific data will be created inside test files
-# - alter baseline
-# - build super specific from scratch
-#
-# >> keep this file as simple as possible.
 
 
 # ------------------------------------------------------------------------------
@@ -68,14 +55,15 @@ config <- design(project = "test",
 item <- "foo"
 
 # -- data.model
+# to pass to some workflow functions
 dm <- yaml_to_dm(config, "name", "type", "default", "class.arg")
-
 
 
 # ------------------------------------------------------------------------------
 # Build items
 # ------------------------------------------------------------------------------
 
+# -- inputs
 values <- list(date = c(NA, "2024-01-14", "2024-01-16", "2024-01-17"),
                name = c("Apple", "Banana", "Mango", "Orange"),
                quantity = c(1, 12, 3, 7),
@@ -83,53 +71,11 @@ values <- list(date = c(NA, "2024-01-14", "2024-01-16", "2024-01-17"),
                isvalid = c(TRUE, FALSE, TRUE, FALSE),
                created = replicate(4, Sys.time()))
 
-# -- build base items
+# -- build items
 items <- values |>
   prepare_values(config, item = "foo") |>
   attribute_values(dm) |>
   rows_insert(data.frame())
-
-# -- items with additional attribute
-# items_extra_att <- items
-#items_extra_att$extra_att <- c("this", "is", "an", "extra")
-
-# -- items without row
-items_no_row <- data.frame("id" = as.numeric(numeric()),
-                           "name" = as.character(character()))
-items_no_row2 <- data.frame("id" = as.numeric(numeric()),
-                            "date" = as.character(character()))
-
-# -- items to test triggers
-#new_item <- list(name = "Raspberry", quantity = 34, total = 86.4, isvalid = TRUE) |> prepare_values(dm) |> attribute_values(dm)
-#update_item <- list(id = items$id[1], name = "Apple-update", quantity = 100, total = 0.1, isvalid = FALSE) |> prepare_values(dm) |> attribute_values(dm)
-#update_item_2 <- list(id = items$id[2], date = NA, name = "Banana-update", quantity = 10, total = 0.1, isvalid = TRUE) |> prepare_values(dm) |> attribute_values(dm)
-
-
-# --------------------------------------------------------------------------
-# Declare miscellaneous parameters
-# --------------------------------------------------------------------------
-
-# -- values to create item
-values <- list("id" = c(170539948621),
-               "date" = c(as.Date("2024-01-25", origin = "01-01-1970")),
-               "name" = c("Orange"),
-               "quantity" = 4,
-               "total" = "78.9",
-               "isvalid" = c(FALSE))
-
-# -- values to create multiple items (same lengths)
-values_multiple <-  list("id" = c(170539948621, 170539948622),
-                         "date" = c(Sys.Date(), Sys.Date()),
-                         "name" = c("name_1", "name_2"),
-                         "quantity" = c(4, 5),
-                         "total" = c(78.9, 80.6),
-                         "isvalid" = c(FALSE, TRUE))
-
-# -- simulate inputs from form
-item_input_values <- list(name = "myname", quantity = 12, total = 34.8)
-
-# -- date selection
-date_slider_value <- c(as.POSIXct(as.Date("2024-01-15")), as.POSIXct(as.Date("2024-01-17")))
 
 
 # --------------------------------------------------------------------------
@@ -139,41 +85,16 @@ date_slider_value <- c(as.POSIXct(as.Date("2024-01-15")), as.POSIXct(as.Date("20
 # -- helper: create test data
 create_testdata <- function(){
 
-  # -- config
+  # folder
+  create_test_folder(testdata_path)
+
+  # config
   config_write(config)
 
-  # -- items
+  # items
   item_save(items,  connector = list(file = items_url))
 
 }
-
-
-# -- helper: create empty items data
-create_empty_items <- function(){
-
-  # -- YAML
-  config_write(c_create(project = "test"))
-
-  # -- save items
-  item_save(items_no_row2,  connector = list(file = items_url))
-
-}
-
-
-# -- helper: create integrity test data
-create_integrity_testdata <- function(){
-
-  # -- alter data model
-  dm <- dm[-3, ]
-
-  # -- save data model
-  saveRDS(dm, file = test_dm_url)
-
-  # -- save items
-  item_save(items,  connector = list(file = items_url))
-
-}
-
 
 # -- helper: cleanup function
 clean_all <- function(){
@@ -181,4 +102,3 @@ clean_all <- function(){
   unlink(testdata_path_base, recursive = TRUE)
 
 }
-
