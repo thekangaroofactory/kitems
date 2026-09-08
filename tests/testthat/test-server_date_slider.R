c14d6f24dd508caf3a30004e9163fccce86b3bbf
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-09-08 11:05:47 +0200

    Fix Date sliderInput #829

--- tests/testthat/test-server_date_slider.R
+++ tests/testthat/test-server_date_slider.R
@@ -23,12 +23,16 @@ test_that("Date sliderInput works", {
     # date
     # --------------------------------------------------------------------------
 
+    # -- get value from the items
+    date_slider_value <- max(items$date, na.rm = T)
+    nb <- nrow(items[items$date == date_slider_value, ])
+
     # -- update input
     session$setInputs(date_slider_strategy = "this-year")
-    session$setInputs(date_slider = date_slider_value)
+    session$setInputs(date_slider = c(date_slider_value, date_slider_value))
 
     # -- check
-    expect_equal(dim(filtered_items()), c(2, 6))
+    expect_equal(nrow(filtered_items()), nb)
 
   })
 
