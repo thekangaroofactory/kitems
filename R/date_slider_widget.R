8abb04770cb6c52aed1c625071e772bfd3c42fe8
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-09-13 10:04:02 +0200

    Fixed #383

--- R/date_slider_widget.R
+++ R/date_slider_widget.R
@@ -6,6 +6,15 @@
 #' The date slider UI component to drive the date filter.
 #'
 #' @param id the server module id.
+#' @param bootstrap optional, a character string for the Bootstrap major version (see details).
+#'
+#' @details
+#' `bootstrap` has been added in version 0.8.0 to determine if `bslib::popover()` can be used
+#' in the widget. The default is provided by `bslib::version_default()`, but there is no obvious
+#' way to know - from the UI side - if the app uses shiny's default or bslib layouts.
+#'
+#' In case you are using shiny's layout (ex: `shiny::fluidPage()`), you should set the parameter
+#' to `bootstrap = "3"`.
 #'
 #' @return An HTML component to include on UI side.
 #' @export
@@ -15,15 +24,14 @@
 #' date_slider_widget(id = "mydata")
 #' }
 
-date_slider_widget <- function(id){
+date_slider_widget <- function(id, bootstrap = bslib::version_default()){
 
   # namespace
   ns <- NS(id)
 
-  # UI
+  # -- wrapper
   tagList(
 
-    # -- slider input
     div(
       style = "display: inline-block; vertical-align:middle; margin-right:40px;",
       sliderInput(inputId = ns("date_slider"),
@@ -33,13 +41,30 @@ date_slider_widget <- function(id){
                   max = Sys.Date(),
                   value = c(Sys.Date(), Sys.Date()))),
 
-    # -- strategies
-    div(
-      style = "display: inline-block; vertical-align:middle;",
-      radioButtons(inputId = ns("date_slider_strategy"),
-                   label = "Strategy",
-                   choices = c("this-year", "keep-range"),
-                   selected = "this-year",
-                   inline = TRUE)))
+    # check Bootstrap version
+    # because bslib::popover requires Bootstrap 5
+    if(bootstrap == "5")
+
+      div(
+        style = "display: inline-block; vertical-align:top;",
+        gear <- bslib::popover(
+          title = "Strategies",
+          placement = "right",
+          trigger = icon("gear"),
+          radioButtons(inputId = ns("date_slider_strategy"),
+                       label = "",
+                       choices = c("this-year", "keep-range"),
+                       selected = "this-year")))
+
+    else
+
+      # (most probably Bootstrap 3)
+      div(
+        style = "display: inline-block; vertical-align:middle;",
+        radioButtons(inputId = ns("date_slider_strategy"),
+                     label = "Strategy",
+                     choices = c("this-year", "keep-range"),
+                     selected = "this-year",
+                     inline = TRUE)))
 
 }
