9ea2bd91b26648bff9bac8ab910f6906c0fb692e
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-09-17 16:50:47 +0200

    refresh #887

--- R/refresh.R
+++ R/refresh.R
@@ -6,25 +6,28 @@
 #' A set of verbs to manipulate the refreshed attribute(s) of an item.
 #'
 #' @param config the config list.
-#' @param item optional (see details), the name (id) of the item group.
+#' @param item the name (id) of the item group.
 #' @param ... the name of the attribute(s) to manipulate.
 #'
 #' @details
-#' `refresh()` and `freeze()` are setter functions that allow to add or remove attributes from the refreshed ones.
-#' `refreshed()` and `frozen()` are getter functions to quickly access the skipped attributes
-#' that are refreshed or not during an item update.
+#' `refresh()` and `freeze()` are setter functions that allow to add or remove
+#' attributes from the refreshed ones.
+#' `refreshed()` and `frozen()` are getter functions to quickly access the skipped
+#' attributes that are refreshed or not during an item update.
 #'
-#' Trying to refresh an attribute that is not skipped will be ignored without warning.
+#' Trying to refresh an attribute that is not skipped will be ignored
+#' *without* a warning.
 #'
 #' When `item` is set in the parent frame, then the attribute can be skipped
 #' in the function call.
 #'
-#' @seealso [parent.frame()]
+#' @seealso [amend()], [parent.frame()]
 #'
-#' @returns a config list (setters) or a character vector (getters)
+#' @returns a list (setters) or a character vector (getters).
 #' @export
 #'
 #' @examples
+#' \dontrun{
 #' # refresh attribute
 #' config <- design(project = "test",
 #' item = "foo") |>
@@ -40,6 +43,7 @@
 #'
 #' # frozen attributes
 #' config |> frozen(item = "foo")
+#' }
 
 refresh <- function(config, item = get_context(), ...){
   ca_behavior(config, item, behavior = "refresh", ...)}
